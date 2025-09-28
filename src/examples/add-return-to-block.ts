20969eaa36c04776a8f1cdbeda3e710628e7afbf
poteat <poteat@users.noreply.github.com>
2025-09-28 14:56:42 -0700

    chore: cleanup example

--- src/examples/add-return-to-block.ts
+++ src/examples/add-return-to-block.ts
@@ -20,21 +20,11 @@ const singleExprBlock = U.BlockStatement({
 
 /** FunctionDeclaration: add return to single-expression body block. */
 export const addReturnToBlock_FunctionDeclaration = U.FunctionDeclaration({
-  id: $,
-  params: $,
-  async: $,
-  generator: $,
-  returnType: $,
-  typeParameters: $,
+  ...$,
   body: singleExprBlock,
 }).to((bag) =>
   U.FunctionDeclaration({
-    id: bag.id,
-    params: bag.params,
-    async: bag.async,
-    generator: bag.generator,
-    returnType: bag.returnType,
-    typeParameters: bag.typeParameters,
+    ...bag,
     body: U.BlockStatement({
       body: [U.ReturnStatement({ argument: bag.body })],
     }),
@@ -43,21 +33,11 @@ export const addReturnToBlock_FunctionDeclaration = U.FunctionDeclaration({
 
 /** FunctionExpression: add return to single-expression body block. */
 export const addReturnToBlock_FunctionExpression = U.FunctionExpression({
-  id: $,
-  params: $,
-  async: $,
-  generator: $,
-  returnType: $,
-  typeParameters: $,
+  ...$,
   body: singleExprBlock,
 }).to((bag) =>
   U.FunctionExpression({
-    id: bag.id,
-    params: bag.params,
-    async: bag.async,
-    generator: bag.generator,
-    returnType: bag.returnType,
-    typeParameters: bag.typeParameters,
+    ...bag,
     body: U.BlockStatement({
       body: [U.ReturnStatement({ argument: bag.body })],
     }),
@@ -67,17 +47,11 @@ export const addReturnToBlock_FunctionExpression = U.FunctionExpression({
 /** ArrowFunctionExpression: add return to single-expression body block. */
 export const addReturnToBlock_ArrowFunctionExpression =
   U.ArrowFunctionExpression({
-    params: $,
-    async: $,
-    returnType: $,
-    typeParameters: $,
+    ...$,
     body: singleExprBlock,
   }).to((bag) =>
     U.ArrowFunctionExpression({
-      params: bag.params,
-      async: bag.async,
-      returnType: bag.returnType,
-      typeParameters: bag.typeParameters,
+      ...bag,
       body: U.BlockStatement({
         body: [U.ReturnStatement({ argument: bag.body })],
       }),
