6eaf88babf815475c69d57f36ff1754a27e35f93
poteat <poteat@users.noreply.github.com>
2025-09-29 19:57:37 -0700

    feat: add elide braces example

---
+++ src/examples/elide-braces-for-return.ts
@@ -0,0 +1,18 @@
+import type { BuilderMap } from "@/ast";
+import { $ } from "@/capture";
+
+declare const U: BuilderMap;
+
+/**
+ * Elide braces for arrow functions that return a single expression.
+ *
+ * Transforms:
+ *   (x) => { return x + 1; }
+ *
+ * Into:
+ *   (x) => x + 1
+ */
+export const elideBracesForReturn = U.BlockStatement({
+  parent: U.ArrowFunctionExpression(),
+  body: [U.ReturnStatement({ argument: $ })],
+}).to(({ argument }) => argument ?? U.Identifier({ name: "undefined" }));
