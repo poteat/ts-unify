bb22a5c3cb3b4b2e5ac5a8010a7ef6c5b37ab6b9
poteat <poteat@users.noreply.github.com>
2025-09-29 20:24:48 -0700

    feat: provide another example

---
+++ src/examples/if-guarded-call-to-optional.ts
@@ -0,0 +1,51 @@
+import type { BuilderMap } from "@/ast";
+import { $ } from "@/capture";
+
+declare const U: BuilderMap;
+
+const callConsequent = U.or(
+  U.BlockStatement({
+    body: [
+      U.ExpressionStatement({
+        expression: U.CallExpression({
+          callee: $("fn"),
+          arguments: $("args"),
+        }),
+      }),
+    ],
+  }),
+  U.ExpressionStatement({
+    expression: U.CallExpression({
+      callee: $("fn"),
+      arguments: $("args"),
+    }),
+  })
+).seal();
+
+/**
+ * Transform if-guarded function calls into optional chaining
+ *
+ * Transforms:
+ *   if (func) {
+ *     func(arg1, arg2);
+ *   }
+ *
+ * Into:
+ *   func?.(arg1, arg2);
+ */
+export const ifGuardedCallToOptional = U.IfStatement({
+  test: $("fn"),
+  consequent: callConsequent,
+  alternate: null,
+})
+  .to(({ fn, args }) =>
+    U.ExpressionStatement({
+      expression: U.ChainExpression({
+        expression: U.CallExpression({
+          callee: fn,
+          arguments: args,
+          optional: true,
+        }),
+      }),
+    })
+  );
